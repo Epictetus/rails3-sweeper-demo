1ec97f0580e1102d85929b93dade2f08c7bc1fd4
philmill <phil@philmill.net>
2011-06-21 16:17:59 -0600

    initial commit with memcached and dalli setup for dev env

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SweeperTest::Application.config.secret_token = 'd6a673aeee7fb9b899bba189ed90f7e2fb78ee2280173c8153eca11dff8db98080c4ed57846589bf457a0ac32c35057976c33dd16a3d97abf3eca64f20109ef9'
